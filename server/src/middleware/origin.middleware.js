0a68e5663b68198b8f0122b8e82b401865af52de
iizaid <businesswebsitezaid54@gmail.com>
2026-05-12 08:17:32 +0300

    Add strict origin middleware to main

---
+++ server/src/middleware/origin.middleware.js
@@ -0,0 +1,34 @@
+import { env } from '../config/env.js';
+import { AppError, errorCodes } from '../utils/AppError.js';
+
+const METHODS_WITH_SIDE_EFFECTS = new Set(['POST', 'PUT', 'PATCH', 'DELETE']);
+
+const normalizeOrigin = (value) => {
+  if (!value || typeof value !== 'string') return null;
+  try {
+    return new URL(value).origin;
+  } catch {
+    return null;
+  }
+};
+
+export const requireAllowedOrigin = (req, _res, next) => {
+  if (!METHODS_WITH_SIDE_EFFECTS.has(req.method)) return next();
+
+  const origin = normalizeOrigin(req.get('origin'));
+  const referer = normalizeOrigin(req.get('referer'));
+  const requestOrigin = origin || referer;
+
+  if (!requestOrigin) {
+    if (env.IS_PRODUCTION) {
+      return next(new AppError(errorCodes.FORBIDDEN, 'Request origin is required.', 403));
+    }
+    return next();
+  }
+
+  if (!env.CLIENT_ORIGINS.includes(requestOrigin)) {
+    return next(new AppError(errorCodes.FORBIDDEN, 'Request origin is not allowed.', 403));
+  }
+
+  return next();
+};
